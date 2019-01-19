b30f75d9fcaf8ec6fe53c977e2e16cf4bea406ad
onino <seba.pinard@gmail.com>
2019-01-19 10:36:22 +0100

    add public

---
+++ public/precache-manifest.53af0dea17a1d606dccd902816d679ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "68e6eb1d52dec85315e311c694de637e",
+    "url": "/static/media/gham-logo2.68e6eb1d.png"
+  },
+  {
+    "revision": "fd3eaf1a97eaae8bc3977cbc69af27cc",
+    "url": "/static/media/gham-logo.fd3eaf1a.png"
+  },
+  {
+    "revision": "35ff0ba3b3ca1c717898bdae3b0ee2ad",
+    "url": "/static/media/bg.35ff0ba3.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b64520f9d1fd38113f8",
+    "url": "/static/js/main.1b64520f.chunk.js"
+  },
+  {
+    "revision": "4746b8750934b025f106",
+    "url": "/static/js/1.4746b875.chunk.js"
+  },
+  {
+    "revision": "1b64520f9d1fd38113f8",
+    "url": "/static/css/main.8f85caa1.chunk.css"
+  },
+  {
+    "revision": "4746b8750934b025f106",
+    "url": "/static/css/1.3659e5e4.chunk.css"
+  },
+  {
+    "revision": "6ed3d6de89b7865d384f3783d9103e8e",
+    "url": "/index.html"
+  }
+];
